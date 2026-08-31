63baffc8d2ebe8c65ff2d37df3efc95a41c5f6a3
root <root@vmi3485677.contaboserver.net>
2026-08-31 18:43:39 +0530

    fix(seo): disallow transient result and rank routes in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/'],
+      disallow: ['/api/', '/result', '/rank', '/review-answerkey'],
     },
     sitemap: 'https://cbtrank.com/sitemap.xml',
   };
