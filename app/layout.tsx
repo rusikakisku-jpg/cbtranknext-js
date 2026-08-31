626e4c70dbf6b9c9c7fe018f31b0b5b1fe0e978a
root <root@vmi3485677.contaboserver.net>
2026-08-31 17:32:57 +0530

    fix(seo): remove duplicate manual favicon link tags from head in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,10 +36,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://api.cbtrank.com" crossOrigin="anonymous" />
