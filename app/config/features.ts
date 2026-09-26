0b1231c66f20a2a4c83f17d5561a8de3cc5355b1
rusikakisku-jpg <rusikakisku@gmail.com>
2026-09-26 10:36:06 +0530

    Update features.ts

--- app/config/features.ts
+++ app/config/features.ts
@@ -20,6 +20,6 @@ export const APP_FEATURE_FLAGS = {
    * Can also be controlled via process.env.SHOW_HOMEPAGE_BLOGS ('true' / 'false').
    */
   SHOW_HOMEPAGE_BLOGS: process.env.SHOW_HOMEPAGE_BLOGS !== undefined
-    ? process.env.SHOW_HOMEPAGE_BLOGS === 'true'
+    ? process.env.SHOW_HOMEPAGE_BLOGS === 'false'
     : true,
 };
