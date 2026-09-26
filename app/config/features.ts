29cf9241f928fe7621ba2865718a671b8ca208b3
Rusika Kisku <rusikakisku.dev@gmail.com>
2026-09-26 05:10:02 +0000

    fix(config): make SHOW_HOMEPAGE_BLOGS a direct boolean flag set to false

--- app/config/features.ts
+++ app/config/features.ts
@@ -17,9 +17,6 @@ export const APP_FEATURE_FLAGS = {
    * Home Page Latest Blogs Section:
    * - Set to true to show latest 5 blog posts on the home page.
    * - Set to false to completely hide the blog posts section from the home page.
-   * Can also be controlled via process.env.SHOW_HOMEPAGE_BLOGS ('true' / 'false').
    */
-  SHOW_HOMEPAGE_BLOGS: process.env.SHOW_HOMEPAGE_BLOGS !== undefined
-    ? process.env.SHOW_HOMEPAGE_BLOGS === 'false'
-    : true,
+  SHOW_HOMEPAGE_BLOGS: false,
 };
